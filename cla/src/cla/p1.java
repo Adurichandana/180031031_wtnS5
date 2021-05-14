7bf5e37f0e2ade4237d458bd0d1522bbc64824f8
dell <dell@DESKTOP-0V4VLF9>
2021-05-14 09:46:58 +0530

    command line arguments

---
+++ cla/src/cla/p1.java
@@ -0,0 +1,10 @@
+package cla;
+public class p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(args[0]+" "+"Technologies"+" "+args[1]);
+
+	}
+
+}
