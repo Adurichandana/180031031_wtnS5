8318486457cb5e8d4ad9761acb201849348664f4
dell <dell@DESKTOP-0V4VLF9>
2021-05-19 10:45:45 +0530

    Strings

---
+++ strings/src/string/s5.java
@@ -0,0 +1,14 @@
+package string;
+
+public class s5 {
+
+
+	public static void main(String[] args) {
+		String str = "Suman";
+		
+		str = str.substring(1, str.length() - 1);
+		
+		System.out.println(str);
+	}
+
+}
